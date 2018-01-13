4c0754e7cd2da07c24225df084eb25732720ae30
Clement M <massardc@users.noreply.github.com>
2018-01-13 21:38:37 +0100

    Moving player's sprite vertically on touch

--- ColorGame/GameScene.swift
+++ ColorGame/GameScene.swift
@@ -21,6 +21,29 @@ class GameScene: SKScene {
         createPlayer()
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if let touch = touches.first {
+            let touchLocation = touch.previousLocation(in: self)
+            let node = self.nodes(at: touchLocation).first
+            
+            if node?.name == "right" {
+                print("Move right")
+            } else if node?.name == "up" {
+                moveVertically(upPressed: true)
+            } else if node?.name == "down" {
+                moveVertically(upPressed: false)
+            }
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        playerSprite?.removeAllActions()
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        playerSprite?.removeAllActions()
+    }
+    
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
@@ -40,5 +63,17 @@ class GameScene: SKScene {
         playerSprite?.position = CGPoint(x: playerXPosition, y: self.size.height / 2)
         
         self.addChild(playerSprite!)
+    }
+    
+    func moveVertically (upPressed: Bool) {
+        if upPressed {
+            let moveAction = SKAction.moveBy(x: 0, y: 3, duration: 0.01)
+            let repeatAction = SKAction.repeatForever(moveAction)
+            playerSprite?.run(repeatAction)
+        } else {
+            let moveAction = SKAction.moveBy(x: 0, y: -3, duration: 0.01)
+            let repeatAction = SKAction.repeatForever(moveAction)
+            playerSprite?.run(repeatAction)
+        }
     }
 }
